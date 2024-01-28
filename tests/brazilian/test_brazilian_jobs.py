23ee1e3b9c60c01a08217b0bf9e118ef71ea3ba4
chapetta <cha.petta@hotmail.com>
2024-01-28 13:24:09 -0300

    Requisito 11 completo: implemente um teste para a funçao read brazilian file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path_mock = 'tests/mocks/brazilians_jobs.csv'
+    result = {'title': 'Maquinista', 'salary': '2000', 'type': 'trainee'}
+    result_2 = {'titulo': 'Maquinista', 'slario': '2000', 'tipo': 'trainee'}
+
+    assert read_brazilian_file(path_mock)[0] == result
+    assert read_brazilian_file(path_mock)[0] != result_2
