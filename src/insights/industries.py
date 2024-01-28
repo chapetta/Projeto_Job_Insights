2e8096289c51787ce73676dede79133542839e4f
chapetta <cha.petta@hotmail.com>
2024-01-28 11:35:12 -0300

    Corrigindo lint

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -12,5 +12,5 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    list_of_industries = [item for item in jobs if item['industry'] == industry]
-    return list_of_industries
+    industries_list = [item for item in jobs if item['industry'] == industry]
+    return industries_list
--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -28,40 +28,32 @@ def get_min_salary(path: str) -> int:
     return min(list_of_min_salaries)
 
 
-def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    
+def validate_salary_fields(job: Dict):
     if 'min_salary' not in job or 'max_salary' not in job:
-        raise ValueError("Both 'min_salary' and 'max_salary' must be present in the job dictionary.")
-    
+        raise ValueError("must be present in the job dictionary.")
+
+
+def validate_numeric_values(value, field_name):
+    if not isinstance(value, (int, str)):
+        raise ValueError("must be a int or a str representing a number.")
+
+
+def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
+    validate_salary_fields(job)
+
     min_salary = job['min_salary']
     max_salary = job['max_salary']
 
-    if type(min_salary) != int or type(max_salary) != int:
-         raise ValueError("Both 'min_salary' and 'max_salary' must be numeric values or strings representing numbers.")
+    validate_numeric_values(min_salary, 'min_salary')
+    validate_numeric_values(max_salary, 'max_salary')
+    validate_numeric_values(salary, 'salary')
+
+    min_salary, max_salary, salary = map(int, [min_salary, max_salary, salary])
+
     if min_salary >= max_salary:
-         raise ValueError("The value of 'min_salary' must be less than or equal to the value of 'max_salary'.")
-    if not isinstance(salary, (int, str)):
-         raise ValueError("The parameter 'salary' must be a numeric value or a string representing a number.")
-    try:
-        salary = int(salary)
-    except ValueError:
-        raise ValueError("Invalid numeric value for 'salary'.")
+        raise ValueError("must be less than the value of 'max_salary'.")
 
     return min_salary <= salary <= max_salary
-    # Returns
-    # -------
-    # bool
-    #     True if the salary is in the salary range of the job, False otherwise
-
-    # Raises
-    # ------
-    # ValueError
-    #     If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-    #     If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-    #     If `job["min_salary"]` is greather than `job["max_salary"]`
-    #     If `salary` isn't a valid integer
-    # """
-    # raise NotImplementedError
 
 
 def filter_by_salary_range(
