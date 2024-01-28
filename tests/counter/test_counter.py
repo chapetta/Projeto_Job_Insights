bc985f477c18a4737ebd505a088dabc34cd453f8
chapetta <cha.petta@hotmail.com>
2024-01-28 13:08:52 -0300

    Requisito 10 completo: implemente um teste para a funçao count ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,12 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'data/jobs.csv'
+
+    assert count_ocurrences(path, 'python') == 1639
+    assert count_ocurrences(path, 'python') != 1633
+    assert count_ocurrences(path, 'javascript') == 122
+    assert count_ocurrences(path, 'zero opções') == 0
+    assert count_ocurrences(path, 'PYTHON') == 1639
+    assert count_ocurrences(path, 'JAVAscript') == 122
